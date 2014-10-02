2d057bbebb9d094f6afa5411a6491280aeff7843
Francis <tjdrms0121@gmail.com>
2014-10-02 15:15:44 +0900

    fail dfa_file

---
+++ compiler/dfa/dfa_file.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#define STATES	3 // number of states
+#define SYMBOLS 2 // number of symbols
+
+char *strFileName = "dfa.txt";
+
+void set_states_table(void){
+	FILE *in;
+	char ch, *str;
+
+	in = fopen("dfa.txt", "r");
+
+	while(fscanf(in, "%c", &ch) == EOF){
+		if(ch == ';'){
+			fscanf(in, "%s", str);
+			printf("%s", *str);
+			continue;
+		}			
+	}	
+}
+
+int ST_TAB[STATES][SYMBOLS] = {
+	{1, 0}, // state P
+	{0, 2}, // state q
+	{-1, 1}	// state r
+};
+
+void put_table(int tab[][SYMBOLS], int m, int n){
+	int i;
+	
+	puts("State Transition Table");
+	puts("\t0\t1");
+	for(i=0; i<m; i++){
+		printf("%c\t%c\t%c\n", 'A'+i, 'A'+tab[i][0], 'A'+tab[i][1]);
+	}
+}
+
+int main(void){
+	int ch; // input symobl
+	int cur_state = 0; // start state
+
+	set_states_table();
+
+	put_table(ST_TAB, STATES, SYMBOLS);
+
+	ch = getchar();
+	while(ch != '\n'){
+		cur_state = ST_TAB[cur_state][ch-'0'];
+		if(cur_state < 0){
+			puts("FAIL");
+			return 0;
+		}	
+		ch = getchar();
+	}
+
+	if(cur_state == 2){
+		puts("OK");
+	}else{
+		puts("FAIL");
+	}
+	
+	return 0;
+}
